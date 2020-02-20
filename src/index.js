4b7c64b24c78b38e601eb563801a9ab96c07f2a2
Peter Slezak <piotrek.slezak@gmail.com>
2020-02-20 22:34:22 +0100

    add Eslint and husky

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+const test = "1";
+
 ReactDOM.render(<App />, document.getElementById('app'));
 
