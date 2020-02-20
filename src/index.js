98af5b8c733dfc69247fa7e343bd5b977c0cac17
Peter Slezak <piotrek.slezak@gmail.com>
2020-02-20 22:35:27 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-const test = "1";
-
 ReactDOM.render(<App />, document.getElementById('app'));
 
