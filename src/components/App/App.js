748bade07161956b8ed9442ae73787ef9bb9f160
Peter Slezak <piotrek.slezak@gmail.com>
2020-02-29 21:09:17 +0100

    add lists iteration to App.js (not working correctly)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
-//import List from '../List/List.js';
+import List from '../List/List.js';
 
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
 
   render() {
-    const {title, subtitle} = this.props;
+    const {title, subtitle, list} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        {/*<List {...listData} />*/}
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
       </main>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,57 +2,34 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+//import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 
 
 class List extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      columns: props.columns || [],
-    };
-  }
-
 
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
     columns: PropTypes.array,
-    imgSrc: PropTypes.string,
+    image: PropTypes.node,
   };
 
   static defaultProps = {
     description: settings.defaultListDescription,
   };
 
-  addColumn(title) {
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const {title, image, description} = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} heroImgSrc={this.props.imgSrc}/>
+        <Hero titleText={title} heroImgSrc={image}/>
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(description)}
         </div>
-        <div className={styles.columns}>
+        {/*<div className={styles.columns}>
           {this.state.columns.map(function (item) {
             //let {cards, title, icon, ...otherProps} = item;
             return <Column key={item.key} cards={item.cards} title={item.title} icon={item.icon}/>;
@@ -60,7 +37,7 @@ class List extends React.Component {
         </div>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div>
+        </div>*/}
       </section>
     );
   }
