891a6aff05a1b305504423ed4c479a6b2d404ee7
Peter Slezak <piotrek.slezak@gmail.com>
2020-05-03 17:06:39 +0200

    cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
   }
 
   render() {
-    const {title, subtitle, list} = this.props;
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
