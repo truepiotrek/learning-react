ba4ec66b099baaf57f15356c2946e5a780e53395
Piotr Slezak <piotrek.slezak@gmail.com>
2021-01-05 14:56:46 +0100

    add Seach component to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
 import List from '../List/ListContainer.js';
-
+import Search from '../Search/SearchContainer.js';
 
 class App extends React.Component {
   static propTypes = {
@@ -20,6 +20,7 @@ class App extends React.Component {
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
+        {Search}
       </main>
     );
   }
